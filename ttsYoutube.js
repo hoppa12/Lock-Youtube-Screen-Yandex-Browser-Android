d1ade0818386305f8ed55c8892430f8e685ea6ef
hoppa12 <troutwaterworld@gmail.com>
2018-04-12 08:44:45 +0100

    Create ttsYoutube.js

---
+++ ttsYoutube.js
@@ -0,0 +1,34 @@
+
+
+
+const youtubeOverlay = ev =>
+{
+    
+    if(/youtube.com\/watch\?/.test(window.location.href))
+    {
+        if(document.querySelector(".customOverlay"))
+        {
+            document.querySelector(".customOverlay").parentElement.remove()
+          
+            return
+        }
+        
+        const hTarget = document.querySelector(".koya-partial")
+        if(ev.target === hTarget)
+        {
+            
+            let newDiv = document.createElement("div"); 
+            newDiv.innerHTML = `<div style="z-index:9999999999!important;top:0!important;left:0!important;height:6000px;width:100vw;background-color:white;position:absolute!important" class="customOverlay"></div>`
+            document.body.appendChild(newDiv)
+         
+
+            return;
+        }
+        
+    }
+  return;
+
+    
+}
+
+document.body.addEventListener("dblclick", youtubeOverlay);
