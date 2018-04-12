656b0bf56adedff1a36ed55eca247fa213850c79
hoppa12 <troutwaterworld@gmail.com>
2018-04-12 09:09:16 +0100

    Update ttsYoutube.js

--- ttsYoutube.js
+++ ttsYoutube.js
@@ -8,18 +8,18 @@ const youtubeOverlay = ev =>
     {
         if(document.querySelector(".customOverlay"))
         {
-            document.querySelector(".customOverlay").parentElement.remove()
+            document.querySelector(".customOverlay").parentElement.remove();
           
-            return
+            return;
         }
         
-        const hTarget = document.querySelector(".koya-partial")
+        const hTarget = document.querySelector(".koya-partial");
         if(ev.target === hTarget)
         {
             
             let newDiv = document.createElement("div"); 
-            newDiv.innerHTML = `<div style="z-index:9999999999!important;top:0!important;left:0!important;height:6000px;width:100vw;background-color:black;position:absolute!important" class="customOverlay"></div>`
-            document.body.appendChild(newDiv)
+            newDiv.innerHTML = `<div style="z-index:9999999999!important;top:0!important;left:0!important;height:6000px;width:100vw;background-color:black;position:absolute!important" class="customOverlay"></div>`;
+            document.body.appendChild(newDiv);
          
 
             return;
